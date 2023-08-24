4115dc1a7f4ce5344b708e69d5666ef8848d994a
Teslyak <124871929+Teslyak@users.noreply.github.com>
2023-08-24 16:03:51 +0300

    react-hw2-task1-feedback

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,30 @@
+import { Component } from 'react';
+
+export class Feedback extends Component {
+    state = {
+  good: 0,
+  neutral: 0,
+  bad: 0
+    }
+    
+    render() {
+        return (
+            
+        <div>
+            <div>
+        <h2>Please live feedback</h2>              
+            <button></button>
+            <button></button>
+            <button></button>
+            </div>
+        <div>
+                </div>
+        <h2>Statistics</h2> 
+                
+                
+            </div >  
+               
+      
+)}
+
+}
